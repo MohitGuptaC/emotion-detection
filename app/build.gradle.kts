52449cb34f113895529e79f062d29d3f09a115e9
MohitGuptaC <155074110+MohitGuptaC@users.noreply.github.com>
2025-06-01 18:15:54 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         }
     }
 
-    /*signingConfigs {
+    signingConfigs {
         // For CI/CD or environment-based signing (e.g., GitHub Actions), use this block:
         create("release") {
             storeFile = file(System.getenv("ORG_GRADLE_PROJECT_storeFile"))
@@ -31,7 +31,7 @@ android {
         }
         // For local Android Studio builds, you can comment out the above block entirely.
         // Android Studio's "Generate Signed Bundle / APK" wizard lets you pick the keystore and credentials interactively.
-    }*/
+    }
 
     buildTypes {
         release {
@@ -42,7 +42,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            //signingConfig = signingConfigs.getByName("release") //comment this line if you want to use Android Studio's interactive signing wizard
+            signingConfig = signingConfigs.getByName("release") //comment this line if you want to use Android Studio's interactive signing wizard
         }
     }
 
